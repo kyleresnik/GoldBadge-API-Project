f173cd2ac8379b4db11ae64cb16e3e221f459d0a
Kyle Resnik <kylebresnik@gmail.com>
2018-10-11 08:45:54 -0400

    second API commit

--- api-project.js
+++ api-project.js
@@ -6,11 +6,10 @@ let url;
 //SEARCH
 const foodSearch = document.querySelector('.search');
 const searchForm = document.querySelector('form');
+searchForm.addEventListener('submit', dataFetch);
 
 //RESULTS
-const section = document.querySelector('section');
-
-searchForm.addEventListener('submit', dataFetch);
+const section = document.querySelector('ul');
 
 //FUNCTIONS
 function dataFetch(e) {
@@ -25,166 +24,25 @@ function dataFetch(e) {
   })
 }
 
-function resultDisplay(json) {
+function resultDisplay(json){
   while (section.firstChild) {
     section.removeChild(section.firstChild);
   }
-  let articles = json.response.docs;
-  if(articles.length === 10) {
-    nav.style.display = 'block';
-  } else {
-    nav.style.display = 'none';
+  for(let i = 0; i < json.hits.length; i++) {
+    let result = json.hits[i].recipe.label;
+    let resultElement = document.createElement("li");
+    let resultLink = document.createElement("a");
+    resultLink.href = json.hits[i].recipe.url;
+    resultLink.innerText = result;
+    // set href to link
+    // set link text to label
+    resultLink.textContent = result;
+    // append link to li
+    resultElement.appendChild(resultLink);
+    // append to section
+    document.getElementById("resultsList").appendChild(resultElement);
   }
-  if(articles.length === 0) {
-    console.log("No results");
-  } else {
-    for(let i = 0; i < articles.length; i++) {
-      let article = document.createElement('article');
-      let heading = document.createElement('h2');
-      let link = document.createElement('a');
-      let img = document.createElement('img');
-      let para = document.createElement('p');
-      let clearfix = document.createElement('div');
-      let current = articles[i];
-      console.log("Current:", current);
-      link.href = current.web_url;
-      link.textContent = current.headline.main;
-      para.textContent = 'Keywords: ';
-      for(let j = 0; j < current.keywords.length; j++) {
-        let span = document.createElement('span');
-        span.textContent += current.keywords[j].value + ' ';
-        para.appendChild(span);
-      }
-      if(current.multimedia.length > 0) {
-        img.src = '' + current.multimedia[0].url;
-        img.alt = current.headline.main;
-      }
-      clearfix.setAttribute('class', 'clearfix');
-      article.appendChild(heading);
-      heading.appendChild(link);
-      article.appendChild(img);
-      article.appendChild(para);
-      article.appendChild(clearfix);
-      section.appendChild(article);
-    }
-  }
-};
-
-function nextPage(e) {
-  pageNumber++;
-  dataFetch(e);
-  console.log("Page number:", pageNumber);
-};
-
-function previousPage(e) {
-  if(pageNumber > 0) {
-    pageNumber--;
-  } else {
-    return;
-  }
-  dataFetch(e);
-  console.log("Page:", pageNumber);
-};
-
-
-// //SEARCH FORM
-// const searchTerm = document.querySelector('.search');
-// const startDate = document.querySelector('.start-date');
-// const endDate = document.querySelector('.end-date');
-// const searchForm = document.querySelector('form');
-// const submitBtn = document.querySelector('.submit');
-
-// //RESULTS NAVIGATION
-// const nextBtn = document.querySelector('.next');
-// const previousBtn = document.querySelector('.prev');
-// const nav = document.querySelector('nav');
-
-// //RESULTS SECTION
-// const section = document.querySelector('section');
-
-// nav.style.display = 'none';
-// let pageNumber = 0;
-// // let displayNav = false;
-   
-// searchForm.addEventListener('submit', fetchResults); 
-// nextBtn.addEventListener('click', nextPage);
-// previousBtn.addEventListener('click', previousPage);
-
-// function fetchResults(e){
-//   e.preventDefault();
-//   url = `${baseURL}?api-key=${key}&page=${pageNumber}&q=${searchTerm.value}`;
-//   console.log("URL:", url);
-//   if(startDate.value !== '') {
-//     console.log(startDate.value)
-//   url += '&begin_date' + startDate.value;
-//   };
-//   if(endDate.value !== '') {
-//     url += '&end_date=' + endDate.value;
-//   };
-//   fetch(url).then(function(result) {
-//     return result.json();
-//   }).then(function(json) {
-//     displayResults(json);
-//   });
-// }
-
-// function displayResults(json) {
-//   while (section.firstChild) {
-//     section.removeChild(section.firstChild);
-//   }
-//   let articles = json.response.docs;
-//   if(articles.length === 10) {
-//     nav.style.display = 'block';
-//   } else {
-//     nav.style.display = 'none';
-//   }
-//   if(articles.length === 0) {
-//     console.log("No results");
-//   } else {
-//     for(let i = 0; i < articles.length; i++) {
-//       let article = document.createElement('article');
-//       let heading = document.createElement('h2');
-//       let link = document.createElement('a');
-//       let img = document.createElement('img');
-//       let para = document.createElement('p');
-//       let clearfix = document.createElement('div');
-//       let current = articles[i];
-//       console.log("Current:", current);
-//       link.href = current.web_url;
-//       link.textContent = current.headline.main;
-//       para.textContent = 'Keywords: ';
-//       for(let j = 0; j < current.keywords.length; j++) {
-//         let span = document.createElement('span');
-//         span.textContent += current.keywords[j].value + ' ';
-//         para.appendChild(span);
-//       }
-//       if(current.multimedia.length > 0) {
-//         img.src = 'http://www.nytimes.com/' + current.multimedia[0].url;
-//         img.alt = current.headline.main;
-//       }
-//       clearfix.setAttribute('class', 'clearfix');
-//       article.appendChild(heading);
-//       heading.appendChild(link);
-//       article.appendChild(img);
-//       article.appendChild(para);
-//       article.appendChild(clearfix);
-//       section.appendChild(article);
-//     }
-//   }
-// };
-
-// function nextPage(e) {
-//   pageNumber++;
-//   fetchResults(e);
-//   console.log("Page number:", pageNumber);
-// };
-
-// function previousPage(e) {
-//   if(pageNumber > 0) {
-//     pageNumber--;
-//   } else {
-//     return;
-//   }
-//   fetchResults(e);
-//   console.log("Page:", pageNumber);
-// };
+  // while (section.firstChild) {
+  //   section.removeChild(section.firstChild);
+  // }
+}
